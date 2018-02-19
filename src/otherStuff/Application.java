15b0ff7483c793e62bb6fb47269750bafb189e74
Farhad Ul Islam <farhad.islam@outlook.com>
2018-02-19 11:32:09 +0000

    Added a new class Application and some static methods

---
+++ src/otherStuff/Application.java
@@ -0,0 +1,43 @@
+package otherStuff;
+
+public class Application {
+
+    private static int numberOfRuns = 0;
+    private static boolean appRunning = false;
+
+    public static void main(String[] args) {
+
+        Application app1 = new Application();
+        app1.start();
+
+        app1.stop();
+
+    }
+
+    public void start() {
+        Application.appRunning = true;
+        System.out.println("App started ...");
+    }
+
+    public void stop() {
+        if(appRunning){
+            appRunning = false;
+        }
+      //appRunning = appRunning ? false : true ;
+        System.out.println("App stopped!");
+    }
+
+    public static void count() {
+        System.out.println("This is a static method");
+        numberOfRuns++;
+    }
+
+    public int getRunningApps() {
+
+        return numberOfRuns;
+    }
+
+
+}
+
+
