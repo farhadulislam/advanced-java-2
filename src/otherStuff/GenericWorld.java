0d054beb449aebdc95bb0b8efc5cf4bb1675fcf3
Farhad Ul Islam <farhad.islam@outlook.com>
2018-02-22 12:17:45 +0000

    GenericWorld updated

--- src/otherStuff/GenericWorld.java
+++ src/otherStuff/GenericWorld.java
@@ -7,21 +7,27 @@ public class GenericWorld {
     public static void main (String [] args){
 
         Generic<String> strObj1 = new Generic<String>("The Alchemist");
-        Generic<Integer> intObj = new Generic<Integer>(2);
+        Generic<Integer> intObj = new Generic<Integer>(2); // Autoboxing
 
-       //List<Integer, Generic<T>> complexList = new ArrayList<Integer, Generic<T>>(2, "Generic Object");
+        Generic<Integer> intObj2 = new Generic<Integer>(Integer.valueOf(2)); // This is another way of writing the above statement.
 
-        System.out.println(strObj1);
-        System.out.println(intObj);
-        //System.out.println(complexList);
+       /*Java compiler does not actually create different versions of Gen, or of any
+        other generic class.  Instead, the compiler removes all generic type information,
+        substituting the necessary casts, to make the code behave
+        as if a specific version of Gen were created. Thus, there is really only one version
+        of Gen that actually exists in the program.
+        The process of removing generic type
+        information is called erasure*/
 
         strObj1.showType();
         intObj.showType();
 
-        int numberFromGenObj = intObj.getGenricObj();//no cast is needed.
+        int numberFromGenObj1 = intObj.getGenricObj();//Auto-unboxing. Therefore, no cast is needed.
+        int numberFromGenOj2 = intObj2.getGenricObj().intValue(); // not using Auto-unboxing
         String bookName = strObj1.getGenricObj();
 
-        System.out.println(numberFromGenObj);
+        System.out.println(numberFromGenObj1);
+        System.out.println(numberFromGenOj2);
         System.out.println(bookName);
 
     }
