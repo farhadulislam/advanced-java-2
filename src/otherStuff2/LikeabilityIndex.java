0404a44f0fef82406e0b384e9eedd45c90dd3010
Farhad Ul Islam <farhad.islam@outlook.com>
2018-03-06 15:28:02 +0000

    Static method for map added; some fixes

--- src/otherStuff2/LikeabilityIndex.java
+++ src/otherStuff2/LikeabilityIndex.java
@@ -9,14 +9,18 @@ public class LikeabilityIndex {
         System.out.println("Instantiating drama object");
         Drama drama1 = new Drama(1, "Patriot");
         Drama drama2 = new Drama(2, "Mirror");
+        Drama drama3 = new Drama(3, "Village");
+        Drama drama4 = new Drama(4, "Postman");
 
         System.out.println("Adding drama records");
         DramaRecords dramaRecords1 = new DramaRecords(drama1, 100, 20, 500);
         DramaRecords dramaRecords2 = new DramaRecords(drama2, 10, 300, 500);
+        DramaRecords dramaRecords3 = new DramaRecords(drama3, 200 , 22, 500000);
+        DramaRecords dramaRecords4 = new DramaRecords(drama4, 1, 3, 50099999);
 
         System.out.println("Stats.....");
-        System.out.println("Number of dramas added so far " + Drama.getNumberOfDramas());
-        System.out.println("Number of drama records added so far " + DramaRecords.getNumOfDramaRecords());
+        System.out.println("Dramas added : " + Drama.getNumberOfDramas());
+        System.out.println("Records added : " + DramaRecords.getNumOfDramaRecords());
 
 
         /**********Collections****/
@@ -25,33 +29,38 @@ public class LikeabilityIndex {
         dramaSet1.add(drama1);
         Map<Drama, DramaRecords> dramaMap1 = new LinkedHashMap<>();
         dramaMap1.put(drama1,dramaRecords1);
+        dramaMap1.put(drama2,dramaRecords2);
+        dramaMap1.put(drama3,dramaRecords3);
+        dramaMap1.put(drama4,dramaRecords4);
 
 
         /***********************/
 
         String index1 = dramaRecords1.toString();
         String index2= dramaRecords2.toString();
+        String index3 = dramaRecords3.toString();
+        String index4= dramaRecords4.toString();
         System.out.println(index1);
         System.out.println(index2);
+        System.out.println(index3);
+        System.out.println(index4);
 
-        showMe(dramaRecords1);
-        showMe(dramaRecords2);
+        showLikability(dramaRecords1);
+        showLikability(dramaRecords2);
+        showLikability(dramaRecords3);
+        showLikability(dramaRecords4);
 
+        System.out.println("Show records using map");
+        showMap(dramaMap1);
 
-
-
-       /* System.out.println(dramaRecords1.getDrama().getDramaName());
-        System.out.println(dramaRecords1.getViews());
-        System.out.println(dramaRecords1.getLikes());
-        System.out.println(dramaRecords1.getDislikes());*/
 
 
     }
 
-    public static void showMe(DramaRecords dramaRecords){
-
+    public static void showLikability(DramaRecords dramaRecords){
+        System.out.println("................");
         System.out.println("For the following drama :- ");
-        System.out.println(dramaRecords.getDrama().getDramaName());
+        System.out.println("**********" + dramaRecords.getDrama().getDramaName() + "**************");
 
         System.out.println("................");
         System.out.println("Returning a value from calcLiability " + dramaRecords.calcLikability1());
@@ -60,4 +69,14 @@ public class LikeabilityIndex {
         System.out.println(" Percentage of likes ratio for this record against views is :  " + dramaRecords.getLikesRatio());
 
     }
+
+    public static void showMap(Map<Drama, DramaRecords> map){
+
+        for (Map.Entry<Drama, DramaRecords> entry : map.entrySet())
+        {
+            System.out.println(entry.getKey().getDramaId() + " : " + entry.getValue());
+        }
+    }
+
+
 }
