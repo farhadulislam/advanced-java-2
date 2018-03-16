ecda0d1ac46ededc582a5ce11cf7029d72f3d4ba
Farhad Ul Islam <farhad.islam@outlook.com>
2018-03-16 14:51:51 +0000

    PASSED testGetArtistFullName

--- Test/dramaRating/ArtistTest.java
+++ Test/dramaRating/ArtistTest.java
@@ -5,8 +5,14 @@ import junit.framework.TestCase;
 public class ArtistTest extends TestCase {
 
     public void testGetArtistFullName() {
-        Artist testArtist = new Artist("first name", "last name");
 
+        Artist testArtist = new Artist("first name", "last name");
+        String firstName = testArtist.getFirstName();
+        String lastName = testArtist.getLastName();
+
+        assertTrue((firstName != null && lastName !=null));
+        assertEquals("first name", testArtist.getFirstName());
+        assertEquals("last name", testArtist.getLastName());
 
     }
 
