2cb2280152ca8e8f31cb2c33e0da6898e1247012
Farhad Ul Islam <farhad.islam@outlook.com>
2018-04-06 11:52:43 +0100

    OtherStuff : added numbers into HashSet, LinkedHashSet, and TreeSet and demonstrated the sorting

--- src/otherStuff/ListSetMapDemo.java
+++ src/otherStuff/ListSetMapDemo.java
@@ -1,28 +1,48 @@
 package otherStuff;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class ListSetMapDemo {
 
-    public static void main (String [] args ){
+    public static void main(String[] args) {
 
         List<Integer> numbers1 = new ArrayList<Integer>();
         numbers1.add(23);
-        numbers1.add(2333);
-        numbers1.add(1);
+        numbers1.add(330);
+        numbers1.add(10);
         numbers1.add(4);
-        numbers1.add(5);
+        numbers1.add(33);
 
-        for(Integer number : numbers1){
-            System.out.println(number);
+        System.out.println("Numbers as entered into ArrayList");
+        System.out.print("ArrayList : ");
+
+        for (Integer number : numbers1) {
+            System.out.print(number + " ");
+
         }
 
 
+            Set<Integer> setOfNumbers1 = new HashSet<>();
+            Set<Integer> setOfNumbers2 = new LinkedHashSet<>();
+            Set<Integer> setOfNumbers3 = new TreeSet<>();
+
+            setOfNumbers1.addAll(numbers1);
+            setOfNumbers2.addAll(numbers1);
+            setOfNumbers3.addAll(numbers1);
+
+            showElementsFromSet(setOfNumbers1, "HashSet");
+            showElementsFromSet(setOfNumbers2, "LinkedHashSet");
+            showElementsFromSet(setOfNumbers3, "TreeSet");
 
 
+        }
 
-
+        public static void showElementsFromSet(Set<Integer> setOfNumbers, String type){
+            System.out.println(" ");
+            System.out.print(type + " : ");
+            for (Integer num : setOfNumbers) {
+                System.out.print(num + " ");
+            }
+        }
     }
-}
+
