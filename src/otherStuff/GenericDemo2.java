6904c5d221fbb014e951117cd3d858cc7b87482c
Farhad Ul Islam <farhad.islam@outlook.com>
2018-04-19 11:35:09 +0100

    OtherStuff: Added a Person demo class to make use of Comparator and compare method to sort age

---
+++ src/otherStuff/GenericDemo2.java
@@ -0,0 +1,31 @@
+package otherStuff;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class GenericDemo2 {
+
+    public static void main(String... args){
+
+        Person p1 = new Person("Adil", 32);
+        Person p2 = new Person("Nayan", 33);
+        Person p3 = new Person("Saikat", 34);
+        Person p4 = new Person("Arijit", 31);
+
+
+        List<Person> personList1 = new ArrayList<>();
+
+        personList1.add(p1);
+        personList1.add(p2);
+        personList1.add(p3);
+        personList1.add(p4);
+
+
+        Collections.sort(personList1, new AgeComparator());
+
+        System.out.println(personList1);
+    }
+}
+
+
